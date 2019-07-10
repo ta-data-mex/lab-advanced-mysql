73431e3fb8eaeda55b0e76a9088803f6c2976f57
uaqro <uaqro@protonmail.com>
2019-07-09 19:05:39 -0500

    results for excercise in solutions.sql

--- your-code/solutions.sql
+++ your-code/solutions.sql
@@ -1,39 +1,19 @@
-SELECT Title, Author, (Royals + Advances) As PROFIT FROM (
-	SELECT Title, Author, SUM(Royals) As Royals, SUM(Advances) As Advances
-		FROM (
-			SELECT ti.title_id as Title, ta.au_id as Author, (ti.price * sales.qty * ti.royalty / 100 * ta.royaltyper / 100) AS Royals, (ti.advance/(ta.royaltyper/100)) AS Advances
-			FROM publications.titles ti
-			JOIN publications.titleauthor ta
-			ON ti.title_id = ta.title_id
-			JOIN publications.sales sales
-			ON ti.title_id = sales.title_id) summary
-		GROUP BY Title) summary
-GROUP BY Author
-ORDER BY PROFIT DESC
-LIMIT 3;
+CREATE TEMPORARY TABLE publications.ro
+SELECT ti.title_id as title_id, (ti.price * (ti.royalty/100) * sum(sales.qty)) AS royalty_calc
+from publications.titles ti
+join publications.sales sales
+on ti.title_id = sales.title_id
+group by ti.title_id
 
-
-CREATE TEMPORARY TABLE publications.authors_profits_step_1
-SELECT ti.title_id as Title, ta.au_id as Author, (ti.price * sales.qty * ti.royalty / 100 * ta.royaltyper / 100) AS Royals, (ti.advance/(ta.royaltyper/100)) AS Advances
+SELECT Author, (Royals + ADV) AS profits FROM (
+SELECT Author, SUM(Royals) as Royals, SUM(Advances) as ADV FROM (
+SELECT ti.title_id as Title, ta.au_id as Author, ro.royalty_calc * (ta.royaltyper / 100) AS Royals, ti.advance*(ta.royaltyper/100) AS Advances
 FROM publications.titles ti
 JOIN publications.titleauthor ta
 ON ti.title_id = ta.title_id
-JOIN publications.sales sales
-ON ti.title_id = sales.title_id;
+JOIN publications.ro ro
+ON ti.title_id = ro.title_id) summary
+GROUP BY Author) summary
+ORDER BY profits DESC
+LIMIT 3
 
-CREATE TEMPORARY TABLE publications.authors_profits_step_2
-SELECT Title, Author, SUM(Royals) As Royals, SUM(Advances) As Advances
-FROM publications.authors_profits_step_1
-GROUP BY Title;
-
-CREATE TEMPORARY TABLE publications.authors_profits_step_3
-SELECT Title, Author, (Royals + Advances) As PROFIT 
-FROM publications.authors_profits_step_2
-GROUP BY Author
-ORDER BY PROFIT DESC
-LIMIT 3;
-
-SELECT * FROM publications.authors_profits_step_3;
-
-CREATE TABLE publications.most_profiting_authors
-SELECT Author as au_id, PROFIT as profits FROM publications.authors_profits_step_3;
