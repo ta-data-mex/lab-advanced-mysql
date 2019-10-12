44aed3c4a1abaf010838c33d72d2f353bbdf1a46
KarenLizzette <karenlizzette.br@gmail.com>
2019-10-11 21:37:26 -0500

    Finished.

---
+++ your-code/challenge_2.sql
@@ -0,0 +1,33 @@
+CREATE TEMPORARY TABLE temp_royalties
+SELECT
+titles.title_id AS title_id,
+authors.au_id AS author_id,
+titles.advance AS advance,
+((titles.price*sales.qty*titles.royalty)/100)*(titleauthor.royaltyper/100) AS royalties
+FROM authors
+JOIN titleauthor ON authors.au_id = titleauthor.au_id
+JOIN titles ON titleauthor.title_id = titles.title_id
+JOIN sales ON titles.title_id = sales.title_id
+;
+SELECT *
+FROM temp_royalties
+;
+CREATE TEMPORARY TABLE temp_per_author
+SELECT 
+title_id,
+author_id,
+advance,
+SUM(royalties) AS total_royalties
+FROM temp_royalties
+GROUP BY author_id, title_id
+;
+SELECT *
+FROM temp_per_author
+;
+SELECT
+author_id,
+advance+total_royalties AS profits
+FROM temp_per_author
+ORDER BY profits DESC
+LIMIT 3
+;
